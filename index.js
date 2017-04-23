8fc4fdb5557dbc28a394e5028b4c604cf76e2867
Denis <carriere.denis@gmail.com>
2017-04-23 15:34:45 -0400

    Undo edits for /index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-var martinez = require('./src/index');
-
-module.exports = {
-  union: martinez.union,
-  diff: martinez.diff,
-  xor: martinez.xor,
-  intersection: martinez.intersection
-};
+module.exports = require('./src/index');
