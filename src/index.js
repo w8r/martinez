d178c5096be3448a4f617e45ee399010b41ed46b
Joel Gallant <joel@joelgallant.me>
2017-07-10 21:34:08 -0600

    Fixes runtime error when adding to result in connectEdges, but array is empty

--- src/index.js
+++ src/index.js
@@ -487,7 +487,11 @@ function connectEdges(sortedEvents) {
     var contour = [[]];
 
     if (!resultEvents[i].isExteriorRing) {
-      result[result.length - 1].push([contour]);
+      if (result.length === 0) {
+        result.push([[contour]]);
+      } else {
+        result[result.length - 1].push([contour]);
+      }
     } else {
       result.push(contour);
     }
