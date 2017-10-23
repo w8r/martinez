608b6aa2ef9346b79a0d428bbc2eba4f9a8c9a96
Rowan Winsemius <rowanwins@yahoo.com.au>
2017-10-24 00:22:47 +1100

    Add multipoly checks

--- test/featureTypes.js
+++ test/featureTypes.js
@@ -16,24 +16,27 @@ var testScenarios = [
   {
     testName: 'polyWithHoleToClipping',
     subjectPoly: 'polyWithHole',
+  },
+  {
+    testName: 'multiPolyToClipping',
+    subjectPoly: 'multiPoly',
   }
 ];
 
 testScenarios.forEach(function (ts) {
   var subject = load.sync(path.join(__dirname, 'featureTypes', ts.subjectPoly + '.geojson'));
-
   tap.test(ts.testName, function (t) {
     var intResult = martinez.intersection(subject.geometry.coordinates, clipping.geometry.coordinates);
-    t.same(intResult[0], load.sync(path.join(outDir, 'intersection', t._name + '.geojson')).geometry.coordinates);
+    t.same(intResult, load.sync(path.join(outDir, 'intersection', t._name + '.geojson')).geometry.coordinates, ts.testName + ' - Intersect');
 
     var xorResult = martinez.xor(subject.geometry.coordinates, clipping.geometry.coordinates);
-    t.same(xorResult, load.sync(path.join(outDir, 'xor', t._name + '.geojson')).geometry.coordinates);
+    t.same(xorResult, load.sync(path.join(outDir, 'xor', t._name + '.geojson')).geometry.coordinates, ts.testName + ' - XOR');
 
     var diffResult = martinez.diff(subject.geometry.coordinates, clipping.geometry.coordinates);
-    t.same(diffResult[0], load.sync(path.join(outDir, 'difference', t._name + '.geojson')).geometry.coordinates);
+    t.same(diffResult, load.sync(path.join(outDir, 'difference', t._name + '.geojson')).geometry.coordinates, ts.testName + ' - Difference');
 
     var unionResult = martinez.union(subject.geometry.coordinates, clipping.geometry.coordinates);
-    t.same(unionResult[0], load.sync(path.join(outDir, 'union', t._name + '.geojson')).geometry.coordinates);
+    t.same(unionResult, load.sync(path.join(outDir, 'union', t._name + '.geojson')).geometry.coordinates, ts.testName + ' - Union');
 
     t.end();
   });
