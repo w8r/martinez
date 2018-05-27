06a43610841ac9bd9900a464ba543a8996fefedb
Alexander Milevski <info@w8r.name>
2018-05-27 22:46:06 +0200

    v0.4.1

--- dist/martinez.umd.js
+++ dist/martinez.umd.js
@@ -1,5 +1,5 @@
 /**
- * martinez v0.4.0
+ * martinez v0.4.1
  * Martinez polygon clipping algorithm, does boolean operation on polygons (multipolygons, polygons with holes etc): intersection, union, difference, xor
  *
  * @author Alex Milevski <info@w8r.name>
