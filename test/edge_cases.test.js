32b86469ad95387e2077b313a36cfaaea0fce1b4
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-01-20 22:06:17 +0100

    removed duplicated tests

--- test/edge_cases.test.js
+++
@@ -1,9 +0,0 @@
-import tap      from 'tape';
-import * as martinez from '../index';
-import load     from 'load-json-file';
-import path     from 'path';
-
-tap.test('Edge cases', (main) => {
-
-    main.end();
-});
