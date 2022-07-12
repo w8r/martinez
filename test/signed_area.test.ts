fb5fc84a57e1c96faa718aeae29d16608e098d14
Alexander Milevski <alexander@linkurio.us>
2022-07-12 22:40:23 +0200

    typings

---
+++ test/signed_area.test.ts
@@ -0,0 +1,23 @@
+// import tap from 'tape';
+import signedArea from '../src/signed_area';
+import { assert } from 'chai';
+
+// tap('analytical signed area', (t) => {
+//   t.equals(signedArea([0, 0], [0, 1], [1, 1]), -1, 'negative area');
+//   t.equals(signedArea([0, 1], [0, 0], [1, 0]), 1, 'positive area');
+//   t.equals(signedArea([0, 0], [1, 1], [2, 2]), 0, 'collinear, 0 area');
+
+//   t.equals(signedArea([-1, 0], [2, 3], [0, 1]), 0, 'point on segment');
+//   t.equals(signedArea([2, 3], [-1, 0], [0, 1]), 0, 'point on segment');
+
+//   t.end();
+// });
+
+describe('signed area', () => {
+  console.log(signedArea);
+  //assert.equal(signedArea([0, 0], [0, 1], [1, 1]), -1, 'negative area');
+  //   t.equals(signedArea([0, 1], [0, 0], [1, 0]), 1, 'positive area');
+  //   t.equals(signedArea([0, 0], [1, 1], [2, 2]), 0, 'collinear, 0 area');
+  //   t.equals(signedArea([-1, 0], [2, 3], [0, 1]), 0, 'point on segment');
+  //   t.equals(signedArea([2, 3], [-1, 0], [0, 1]), 0, 'point on segment');
+});
