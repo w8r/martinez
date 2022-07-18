4fc1f5c43feb5a0c3e1e2e077c3b8126e778ff31
Alexander Milevski <alexander@linkurio.us>
2022-07-18 23:24:16 +0200

    segment intersection test

--- test/segment_intersection.test.ts
+++ test/segment_intersection.test.ts
@@ -1,155 +1,144 @@
-import tap from 'tape';
+import { expect, test } from 'vitest';
 import intersection from '../src/segment_intersection';
 
-tap('intersection', (t) => {
-  t.deepEqual(
+test('intersection', (t) => {
+  expect(
     intersection([0, 0], [1, 1], [1, 0], [2, 2], false),
-    null,
     'null if no intersections'
-  );
-  t.deepEqual(
+  ).toBeNull();
+
+  expect(
     intersection([0, 0], [1, 1], [1, 0], [10, 2], false),
-    null,
     'null if no intersections'
-  );
-  t.deepEqual(
+  ).toBeNull();
+
+  expect(
     intersection([2, 2], [3, 3], [0, 6], [2, 4], false),
-    null,
     'null if no intersections'
-  );
+  ).toBeNull();
 
-  t.deepEqual(
+  expect(
     intersection([0, 0], [1, 1], [1, 0], [0, 1], false),
-    [[0.5, 0.5]],
     '1 intersection'
-  );
-  t.deepEqual(
+  ).toStrictEqual([[0.5, 0.5]]);
+
+  expect(
     intersection([0, 0], [1, 1], [0, 1], [0, 0], false),
-    [[0, 0]],
     'shared point 1'
-  );
-  t.deepEqual(
+  ).toStrictEqual([[0, 0]]);
+
+  expect(
     intersection([0, 0], [1, 1], [0, 1], [1, 1], false),
-    [[1, 1]],
     'shared point 2'
-  );
+  ).toStrictEqual([[1, 1]]);
 
-  t.deepEqual(
+  expect(
     intersection([0, 0], [1, 1], [0.5, 0.5], [1, 0], false),
-    [[0.5, 0.5]],
     'T-crossing'
-  );
+  ).toStrictEqual([[0.5, 0.5]]);
 
-  t.deepEqual(
+  expect(
     intersection([0, 0], [10, 10], [1, 1], [5, 5], false),
-    [
-      [1, 1],
-      [5, 5]
-    ],
     'full overlap'
-  );
-  t.deepEqual(
+  ).toStrictEqual([
+    [1, 1],
+    [5, 5]
+  ]);
+
+  expect(
     intersection([1, 1], [10, 10], [1, 1], [5, 5], false),
-    [
-      [1, 1],
-      [5, 5]
-    ],
     'shared point + overlap'
-  );
-  t.deepEqual(
+  ).toStrictEqual([
+    [1, 1],
+    [5, 5]
+  ]);
+
+  expect(
     intersection([3, 3], [10, 10], [0, 0], [5, 5], false),
-    [
-      [3, 3],
-      [5, 5]
-    ],
     'mutual overlap'
-  );
-  t.deepEqual(
+  ).toStrictEqual([
+    [3, 3],
+    [5, 5]
+  ]);
+
+  expect(
     intersection([0, 0], [1, 1], [0, 0], [1, 1], false),
-    [
-      [0, 0],
-      [1, 1]
-    ],
     'full overlap'
-  );
-  t.deepEqual(
+  ).toStrictEqual([
+    [0, 0],
+    [1, 1]
+  ]);
+
+  expect(
     intersection([1, 1], [0, 0], [0, 0], [1, 1], false),
-    [
-      [1, 1],
-      [0, 0]
-    ],
     'full overlap, orientation'
-  );
+  ).toStrictEqual([
+    [1, 1],
+    [0, 0]
+  ]);
 
-  t.deepEqual(
+  expect(
     intersection([0, 0], [1, 1], [1, 1], [2, 2], false),
-    [[1, 1]],
     'collinear, shared point'
-  );
-  t.deepEqual(
+  ).toStrictEqual([[1, 1]]);
+
+  expect(
     intersection([1, 1], [0, 0], [1, 1], [2, 2], false),
-    [[1, 1]],
+
     'collinear, shared other point'
-  );
-  t.deepEqual(
+  ).toStrictEqual([[1, 1]]);
+  expect(
     intersection([0, 0], [1, 1], [2, 2], [4, 4], false),
-    null,
     'collinear, no overlap'
-  );
-  t.deepEqual(
+  ).toBeNull();
+
+  expect(
     intersection([0, 0], [1, 1], [0, -1], [1, 0], false),
-    null,
     'parallel'
-  );
-  t.deepEqual(
+  ).toBeNull();
+
+  expect(
     intersection([1, 1], [0, 0], [0, -1], [1, 0], false),
-    null,
     'parallel, orientation'
-  );
-  t.deepEqual(
+  ).toBeNull();
+
+  expect(
     intersection([0, -1], [1, 0], [0, 0], [1, 1], false),
-    null,
     'parallel, position'
-  );
+  ).toBeNull();
 
-  t.deepEqual(
+  expect(
     intersection([0, 0], [1, 1], [0, 1], [0, 0], true),
-    null,
     'shared point 1, skip touches'
-  );
-  t.deepEqual(
+  ).toBeNull();
+
+  expect(
     intersection([0, 0], [1, 1], [0, 1], [1, 1], true),
-    null,
     'shared point 2, skip touches'
-  );
+  ).toBeNull();
 
-  t.deepEqual(
+  expect(
     intersection([0, 0], [1, 1], [1, 1], [2, 2], true),
-    null,
     'collinear, shared point, skip touches'
-  );
-  t.deepEqual(
+  ).toBeNull();
+
+  expect(
     intersection([1, 1], [0, 0], [1, 1], [2, 2], true),
-    null,
     'collinear, shared other point, skip touches'
-  );
+  ).toBeNull();
 
-  t.deepEqual(
+  expect(
     intersection([0, 0], [1, 1], [0, 0], [1, 1], true),
-    null,
     'full overlap, skip touches'
-  );
-  t.deepEqual(
+  ).toBeNull();
+
+  expect(
     intersection([1, 1], [0, 0], [0, 0], [1, 1], true),
-    null,
     'full overlap, orientation, skip touches'
-  );
+  ).toBeNull();
 
-  t.deepEqual(
+  expect(
     intersection([0, 0], [1, 1], [1, 0], [0, 1], true),
-    [[0.5, 0.5]],
     '1 intersection, skip touches'
-  );
-
-  t.end();
+  ).toStrictEqual([[0.5, 0.5]]);
 });
