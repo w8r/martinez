0515df7baaab4c5e25bdf50096ee4798767ab0bf
w8r <w8r@users.noreply.github.com>
2022-07-24 19:29:35 +0000

    Deploying to gh-pages from @ w8r/martinez@49e859ca8aa4328ad8cff2a43af7fc9a5d1211fd 🚀

--- src/edge_type.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  NORMAL:               0,
-  NON_CONTRIBUTING:     1,
-  SAME_TRANSITION:      2,
-  DIFFERENT_TRANSITION: 3
-};
--- test/compute_fields.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var tap = require('tap');
-// var martinez = require('../src/');
-// var computeFields = martinez.computeFields;
-
-tap.test('compute fields', function (t) {
-  t.end();
-});
