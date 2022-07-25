d3213dcd202d60e1ca348ee2fffe84a080d39268
Alexander Milevski <alexander@linkurio.us>
2022-07-25 20:22:21 +0200

    Cleanup

--- bench.js
+++ bench.js
@@ -1,8 +1,8 @@
-import load      from 'load-json-file';
+/* eslint-disable no-console */
+import load from 'load-json-file';
 import Benchmark from 'benchmark';
 import jstsUnion from '@turf/union';
-import martinez  from './dist/martinez.umd';
-
+import * as martinez from './dist/martinez.es.js';
 
 /**
  * Benmark Results
@@ -12,25 +12,31 @@ import martinez  from './dist/martinez.umd';
  * Asia x 6.32 ops/sec ±3.16% (20 runs sampled)
  * Asia - JSTS x 6.62 ops/sec ±2.74% (21 runs sampled)
  */
-
 const options = {
-  onStart (event) { console.log(this.name); },
-  onError (event) { console.log(event.target.error); },
-  onCycle (event) { console.log(String(event.target)); },
+  onStart() {
+    console.log(this.name);
+  },
+  onError(event) {
+    console.log(event.target.error);
+  },
+  onCycle(event) {
+    console.log(String(event.target));
+  },
   onComplete() {
     console.log('- Fastest is ' + this.filter('fastest').map('name') + '\n');
   }
 };
 
-const hole_hole = load.sync('./test/fixtures/hole_hole.geojson');
+const holeHole = load.sync('./test/fixtures/hole_hole.geojson');
 new Benchmark.Suite('Hole_Hole', options)
   .add('Martinez', () => {
     martinez.union(
-      hole_hole.features[0].geometry.coordinates,
-      hole_hole.features[1].geometry.coordinates);
+      holeHole.features[0].geometry.coordinates,
+      holeHole.features[1].geometry.coordinates
+    );
   })
   .add('JSTS', () => {
-    jstsUnion(hole_hole.features[0], hole_hole.features[1]);
+    jstsUnion(holeHole.features[0], holeHole.features[1]);
   })
   .run();
 
@@ -40,7 +46,8 @@ new Benchmark.Suite('Asia union', options)
   .add('Martinez', () => {
     martinez.union(
       asia.features[0].geometry.coordinates,
-      unionPoly.geometry.coordinates);
+      unionPoly.geometry.coordinates
+    );
   })
   .add('JSTS', () => jstsUnion(asia.features[0], unionPoly))
   .run();
@@ -50,7 +57,8 @@ new Benchmark.Suite('States clip', options)
   .add('Martinez', () => {
     martinez.union(
       states.features[0].geometry.coordinates,
-      states.features[1].geometry.coordinates);
+      states.features[1].geometry.coordinates
+    );
   })
   .add('JSTS', () => {
     jstsUnion(states.features[0], states.features[1]);
